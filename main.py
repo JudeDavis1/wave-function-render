ddc641c6f57fde9b978383be423d41384fcc0db2
Jude Davis (40191355) <40191355@hcuc.ac.uk>
2023-03-01 10:55:10 +0000

    feat: now using the energy property of the wave function

--- main.py
+++ main.py
@@ -11,32 +11,42 @@ y_set = []
 z_set = []
 
 PLANKS_CONSTANT = 6.626 * 10**-34
-PLANKS_CONSTANT_BAR = PLANKS_CONSTANT / (2 * np.pi)
+H_BAR = PLANKS_CONSTANT / (2 * np.pi)
+DELTA = 0.07
 
 def animate(frame_i):
     """Animation function"""
 
-    x = 0.1 * frame_i
-    y = wave_func(x, t=time.time(), n_=x/2)
+    x = DELTA * frame_i
+    y = wave_func(x, t=time.time(), n_=x / 2)
 
     x_set.append(x)
     y_set.append(y)
 
     ax.clear()
-    ax.scatter(x_set, y_set, x_set)
+    ax.scatter(x_set, y_set, x_set, s=4)
 
-def wave_func(x, t, a=4, n_=5, eN=0.4):
-    """Wave function solved by Schrodinger's equation"""
+def wave_func(x, t, a=4, n_=5, m=1.00784):
+    """Wave function by solving Schrodinger's equation"""
+
+    eN = get_energy(n_, m, a)
     norm_term = np.sqrt(2 / a)
-    ret = norm_term * np.sin(n_ * np.pi * x / a) * np.exp(-1j * eN * t / PLANKS_CONSTANT_BAR)
+    time_dependence = np.exp(-1j * eN * t / H_BAR)
+    ret = norm_term * np.sin(n_ * np.pi * x / a) * time_dependence
 
     return ret
+
+def get_energy(n, m, a):
+    potential = 0
+    upper = np.product([H_BAR ** 2, np.pi ** 2, n ** 2])
+    lower = 2 * m * a ** 2
+    return (upper / lower) + potential
 
 anim = animation.FuncAnimation(
     fig,
     animate,
-    interval=1,
-    frames=int(100),
+    interval=0,
+    frames=int(1e6),
 
 )
 plt.show()
